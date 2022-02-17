7ba7ae185be1db27e06ea24483f51162758b5fdf
meows <b5c6@protonmail.com>
2022-02-17 12:51:34 -0800

    fix deploy
    Signed-off-by: meows <b5c6@protonmail.com>

--- deploy-isaac.sh
+++ deploy-isaac.sh
@@ -4,6 +4,6 @@ set -e
 set -x
 
 mkdir -p ./build/bin
-go build -o ./build/bin/rpc2influx ./v2
+go build -o ./build/bin/rpc2influx .
 rsync -avz ./build/bin/rpc2influx coop-do-ethercluster-metrics:/usr/local/bin/
 
